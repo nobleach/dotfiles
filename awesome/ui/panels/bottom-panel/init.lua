a4379bf6e805351d286c88976092c49cddfe37e7
Jim Wharton <jamesawharton@gmail.com>
2022-09-10 07:45:05 -0600

    pre battery/layout

--- awesome/ui/panels/bottom-panel/init.lua
+++ awesome/ui/panels/bottom-panel/init.lua
@@ -312,6 +312,40 @@ return function(s)
 		return widget
 	end
 
+	--- Layoutbox
+	--- ~~~~~~~~~
+	local function layoutbox()
+		local layoutbox_buttons = gears.table.join(
+			--- Left click
+			awful.button({}, 1, function(c)
+				awful.layout.inc(1)
+			end),
+
+			--- Right click
+			awful.button({}, 3, function(c)
+				awful.layout.inc(-1)
+			end),
+
+			--- Scrolling
+			awful.button({}, 4, function()
+				awful.layout.inc(-1)
+			end),
+			awful.button({}, 5, function()
+				awful.layout.inc(1)
+			end)
+		)
+
+		s.mylayoutbox = awful.widget.layoutbox()
+		s.mylayoutbox:buttons(layoutbox_buttons)
+
+		local widget = wbutton.elevated.state({
+			child = s.mylayoutbox,
+			normal_bg = beautiful.wibar_bg,
+		})
+
+		return widget
+    end
+
 	
 	--- Create the top_panel
 	--- ~~~~~~~~~~~~~~~~~~~~~~~
