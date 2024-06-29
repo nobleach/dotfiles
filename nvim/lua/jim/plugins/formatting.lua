502547a9a5143972267f526a7c9cd76d94be01c6
Jim Wharton <jim.wharton@ww.com>
2024-06-28 21:25:48 -0400

    chore: add script for biome

--- nvim/lua/jim/plugins/formatting.lua
+++ nvim/lua/jim/plugins/formatting.lua
@@ -5,16 +5,44 @@ return {
 	config = function()
 		local conform = require("conform")
 
+		local function biome_lsp_or_prettier(bufnr)
+			local has_biome_lsp = vim.lsp.get_active_clients({
+				bufnr = bufnr,
+				name = "biome",
+			})[1]
+			if has_biome_lsp then
+				return {}
+			end
+			local has_prettier = vim.fs.find({
+				-- https://prettier.io/docs/en/configuration.html
+				".prettierrc",
+				".prettierrc.json",
+				".prettierrc.yml",
+				".prettierrc.yaml",
+				".prettierrc.json5",
+				".prettierrc.js",
+				".prettierrc.cjs",
+				".prettierrc.toml",
+				"prettier.config.js",
+				"prettier.config.cjs",
+			}, { upward = true })[1]
+			if has_prettier then
+				return { "prettier" }
+			end
+			return { "biome" }
+		end
+
 		conform.setup({
 			formatters_by_ft = {
-				javascript = { "prettier" },
-				typescript = { "prettier" },
-				javascriptreact = { "prettier" },
-				typescriptreact = { "prettier" },
+				javascript = biome_lsp_or_prettier,
+				typescript = biome_lsp_or_prettier,
+				javascriptreact = biome_lsp_or_prettier,
+				typescriptreact = biome_lsp_or_prettier,
+				json = { "biome" },
+				jsonc = { "biome" },
 				svelte = { "prettier" },
 				css = { "prettier" },
 				html = { "prettier" },
-				json = { "prettier" },
 				yaml = { "prettier" },
 				markdown = { "prettier" },
 				graphql = { "prettier" },
