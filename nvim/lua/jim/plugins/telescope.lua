277e648ee50c73f933735a8befca660ab58eff01
Jim Wharton <jim.wharton@ww.com>
2024-08-15 11:02:46 -0400

    chore: add git shortcuts

--- nvim/lua/jim/plugins/telescope.lua
+++ nvim/lua/jim/plugins/telescope.lua
@@ -46,6 +46,12 @@ return {
 		)
 		keymap.set(
 			"n",
+			";b",
+			"<cmd>lua require('telescope.builtin').git_branches()<cr>",
+			{ desc = "List all GIT branches" }
+		)
+		keymap.set(
+			"n",
 			"<leader>tgc",
 			"<cmd>lua require('telescope.builtin').git_commits()<cr>",
 			{ desc = "List all GIT commits" }
