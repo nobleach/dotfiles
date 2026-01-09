74cc3cfa183ea8ef01f3f24f92493603d194bcda
Jim Wharton <jim.wharton@ww.com>
2026-01-09 10:41:55 -0500

    update: update and reconfig tree sitter

--- nvim/lua/jim/plugins/nvim-treesitter.lua
+++ nvim/lua/jim/plugins/nvim-treesitter.lua
@@ -3,29 +3,28 @@ return {
 		"nvim-treesitter/nvim-treesitter",
 		event = { "BufReadPre", "BufNewFile" },
 		build = ":TSUpdate",
+		branch = "main",
 		dependencies = {
-			"nvim-treesitter/nvim-treesitter-textobjects",
 			"windwp/nvim-ts-autotag",
 		},
 		config = function()
 			-- import nvim-treesitter plugin
-			local treesitter = require("nvim-treesitter.configs")
 			vim.treesitter.language.register("markdown", "mdx")
 
-      local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-      parser_config.d2 = {
-        install_info = {
-          url = 'https://git.pleshevski.ru/pleshevskiy/tree-sitter-d2',
-          revision = 'main',
-          files = { 'src/parser.c', 'src/scanner.cc' },
-        },
-        filetype = 'd2',
-      };
+			-- local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+			-- parser_config.d2 = {
+			-- 	install_info = {
+			-- 		url = "https://git.pleshevski.ru/pleshevskiy/tree-sitter-d2",
+			-- 		revision = "main",
+			-- 		files = { "src/parser.c", "src/scanner.cc" },
+			-- 	},
+			-- 	filetype = "d2",
+			-- }
 
 			-- configure treesitter
-			treesitter.setup({ -- enable syntax highlighting
+			require("nvim-treesitter").setup({
 				highlight = {
-					enable = true,
+					-- enable = true,
 				},
 				-- enable indentation
 				indent = { enable = true },
@@ -54,15 +53,6 @@ return {
 					"dockerfile",
 					"gitignore",
 					"query",
-				},
-				incremental_selection = {
-					enable = true,
-					keymaps = {
-						init_selection = "<C-space>",
-						node_incremental = "<C-space>",
-						scope_incremental = false,
-						node_decremental = "<bs>",
-					},
 				},
 				-- enable nvim-ts-context-commentstring plugin for commenting tsx and jsx
 				context_commentstring = {
