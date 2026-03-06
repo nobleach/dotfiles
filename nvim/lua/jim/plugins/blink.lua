996cfabcc7e28a3ed7de02382ec1ba76a16a521f
nobleach <jamesawharton@gmail.com>
2026-03-06 10:43:34 -0500

    feat: add escape from snippets

--- nvim/lua/jim/plugins/blink.lua
+++ nvim/lua/jim/plugins/blink.lua
@@ -7,6 +7,22 @@ return {
 			require("luasnip.loaders.from_lua").lazy_load({ paths = "~/.config/nvim/snippets/" })
 			require("luasnip.loaders.from_vscode").lazy_load()
 			require("luasnip.loaders.from_vscode").lazy_load({ paths = { vim.fn.stdpath("config") .. "/snippets" } })
+
+			-- Fix LuaSnip getting stuck in a snippet after leaving Insert mode
+			function leave_snippet()
+				if
+					((vim.v.event.old_mode == "s" and vim.v.event.new_mode == "n") or vim.v.event.old_mode == "i")
+					and require("luasnip").session.current_nodes[vim.api.nvim_get_current_buf()]
+					and not require("luasnip").session.jump_active
+				then
+					require("luasnip").unlink_current()
+				end
+			end
+
+			-- stop snippets when you leave to normal mode
+			vim.api.nvim_command([[
+        autocmd ModeChanged * lua leave_snippet()
+      ]])
 		end,
 	},
 
