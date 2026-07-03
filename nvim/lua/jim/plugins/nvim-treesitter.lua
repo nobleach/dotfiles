8984eff417c3b2a5ec5b467b82f603a1a342ca57
Jim Wharton <jim.wharton@ww.com>
2026-07-03 11:11:31 -0400

    update: attempt to get better treesitter coloring

--- nvim/lua/jim/plugins/nvim-treesitter.lua
+++ nvim/lua/jim/plugins/nvim-treesitter.lua
@@ -5,4 +5,17 @@ return {
 	dependencies = { "neovim-treesitter/treesitter-parser-registry" },
 	lazy = false,
 	build = ":TSUpdate",
+	config = function()
+		require("nvim-treesitter").install({ "go", "rust", "python", "typescript" })
+
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = { "rust", "python", "typescript" },
+			callback = function()
+				vim.treesitter.start() -- highlighting
+				vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()" -- folds
+				vim.wo.foldmethod = "expr"
+				vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()" -- indentation
+			end,
+		})
+	end,
 }
