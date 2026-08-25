a8cbdd4bc309137c0969154b8d352cdfa6cb809f
Jim Wharton <jim.wharton@ww.com>
2026-08-25 19:35:15 -0400

    chore: add yanky

---
+++ nvim/lua/jim/plugins/yanky.lua
@@ -0,0 +1,31 @@
+return {
+	"gbprod/yanky.nvim",
+	opts = {
+		-- your configuration comes here
+		-- or leave it empty to use the default settings
+		-- refer to the configuration section below
+	},
+	config = function(_, opts)
+		require("yanky").setup(opts)
+
+		vim.keymap.set({ "n", "x" }, "p", "<Plug>(YankyPutAfter)")
+		vim.keymap.set({ "n", "x" }, "P", "<Plug>(YankyPutBefore)")
+		vim.keymap.set({ "n", "x" }, "gp", "<Plug>(YankyGPutAfter)")
+		vim.keymap.set({ "n", "x" }, "gP", "<Plug>(YankyGPutBefore)")
+
+		vim.keymap.set("n", "<c-u>", "<Plug>(YankyPreviousEntry)")
+		vim.keymap.set("n", "<c-i>", "<Plug>(YankyNextEntry)")
+		vim.keymap.set("n", "]p", "<Plug>(YankyPutIndentAfterLinewise)")
+		vim.keymap.set("n", "[p", "<Plug>(YankyPutIndentBeforeLinewise)")
+		vim.keymap.set("n", "]P", "<Plug>(YankyPutIndentAfterLinewise)")
+		vim.keymap.set("n", "[P", "<Plug>(YankyPutIndentBeforeLinewise)")
+
+		vim.keymap.set("n", ">p", "<Plug>(YankyPutIndentAfterShiftRight)")
+		vim.keymap.set("n", "<p", "<Plug>(YankyPutIndentAfterShiftLeft)")
+		vim.keymap.set("n", ">P", "<Plug>(YankyPutIndentBeforeShiftRight)")
+		vim.keymap.set("n", "<P", "<Plug>(YankyPutIndentBeforeShiftLeft)")
+
+		vim.keymap.set("n", "=p", "<Plug>(YankyPutAfterFilter)")
+		vim.keymap.set("n", "=P", "<Plug>(YankyPutBeforeFilter)")
+	end,
+}
